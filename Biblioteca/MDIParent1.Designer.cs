9996d639ab2fb5977bcf7bd5b05a9b7153fdd64c
Kauã Araujo Casimiro <kaua.casimiro16@gmail.com>
2024-04-17 00:16:18 -0300

    Formulário Principal

---
+++ Biblioteca/MDIParent1.Designer.cs
@@ -0,0 +1,219 @@
+﻿
+namespace Biblioteca
+{
+    partial class MDIParent1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.menuStrip = new System.Windows.Forms.MenuStrip();
+            this.statusStrip = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolTip = new System.Windows.Forms.ToolTip(this.components);
+            this.cadastroGeralToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.editarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.consultarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ajudaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.opçõesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.backupToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.restaurarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cadastroLivroToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cadastroFuncionárioToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cadastroLocatárioToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cadastroLocaçãoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.editoraToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip.SuspendLayout();
+            this.statusStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip
+            // 
+            this.menuStrip.Font = new System.Drawing.Font("Consolas", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.menuStrip.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.cadastroGeralToolStripMenuItem,
+            this.editarToolStripMenuItem,
+            this.consultarToolStripMenuItem,
+            this.ajudaToolStripMenuItem,
+            this.opçõesToolStripMenuItem});
+            this.menuStrip.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip.Name = "menuStrip";
+            this.menuStrip.Padding = new System.Windows.Forms.Padding(9, 2, 0, 2);
+            this.menuStrip.Size = new System.Drawing.Size(948, 28);
+            this.menuStrip.TabIndex = 0;
+            this.menuStrip.Text = "MenuStrip";
+            // 
+            // statusStrip
+            // 
+            this.statusStrip.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.statusStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel});
+            this.statusStrip.Location = new System.Drawing.Point(0, 591);
+            this.statusStrip.Name = "statusStrip";
+            this.statusStrip.Padding = new System.Windows.Forms.Padding(1, 0, 21, 0);
+            this.statusStrip.Size = new System.Drawing.Size(948, 26);
+            this.statusStrip.TabIndex = 2;
+            this.statusStrip.Text = "StatusStrip";
+            // 
+            // toolStripStatusLabel
+            // 
+            this.toolStripStatusLabel.Name = "toolStripStatusLabel";
+            this.toolStripStatusLabel.Size = new System.Drawing.Size(49, 20);
+            this.toolStripStatusLabel.Text = "Status";
+            // 
+            // toolTip
+            // 
+            this.toolTip.Popup += new System.Windows.Forms.PopupEventHandler(this.toolTip_Popup);
+            // 
+            // cadastroGeralToolStripMenuItem
+            // 
+            this.cadastroGeralToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.cadastroLivroToolStripMenuItem,
+            this.cadastroFuncionárioToolStripMenuItem,
+            this.cadastroLocatárioToolStripMenuItem,
+            this.cadastroLocaçãoToolStripMenuItem,
+            this.editoraToolStripMenuItem});
+            this.cadastroGeralToolStripMenuItem.Name = "cadastroGeralToolStripMenuItem";
+            this.cadastroGeralToolStripMenuItem.Size = new System.Drawing.Size(149, 24);
+            this.cadastroGeralToolStripMenuItem.Text = "Cadastro Geral";
+            // 
+            // editarToolStripMenuItem
+            // 
+            this.editarToolStripMenuItem.Name = "editarToolStripMenuItem";
+            this.editarToolStripMenuItem.Size = new System.Drawing.Size(77, 24);
+            this.editarToolStripMenuItem.Text = "Editar";
+            // 
+            // consultarToolStripMenuItem
+            // 
+            this.consultarToolStripMenuItem.Name = "consultarToolStripMenuItem";
+            this.consultarToolStripMenuItem.Size = new System.Drawing.Size(104, 24);
+            this.consultarToolStripMenuItem.Text = "Consultar";
+            // 
+            // ajudaToolStripMenuItem
+            // 
+            this.ajudaToolStripMenuItem.Name = "ajudaToolStripMenuItem";
+            this.ajudaToolStripMenuItem.Size = new System.Drawing.Size(68, 24);
+            this.ajudaToolStripMenuItem.Text = "Ajuda";
+            // 
+            // opçõesToolStripMenuItem
+            // 
+            this.opçõesToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.backupToolStripMenuItem,
+            this.restaurarToolStripMenuItem});
+            this.opçõesToolStripMenuItem.Name = "opçõesToolStripMenuItem";
+            this.opçõesToolStripMenuItem.Size = new System.Drawing.Size(77, 24);
+            this.opçõesToolStripMenuItem.Text = "Opções";
+            // 
+            // backupToolStripMenuItem
+            // 
+            this.backupToolStripMenuItem.Name = "backupToolStripMenuItem";
+            this.backupToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.backupToolStripMenuItem.Text = "Backup";
+            // 
+            // restaurarToolStripMenuItem
+            // 
+            this.restaurarToolStripMenuItem.Name = "restaurarToolStripMenuItem";
+            this.restaurarToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.restaurarToolStripMenuItem.Text = "Restaurar";
+            // 
+            // cadastroLivroToolStripMenuItem
+            // 
+            this.cadastroLivroToolStripMenuItem.Name = "cadastroLivroToolStripMenuItem";
+            this.cadastroLivroToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.cadastroLivroToolStripMenuItem.Text = "Livro";
+            this.cadastroLivroToolStripMenuItem.Click += new System.EventHandler(this.cadastroLivroToolStripMenuItem_Click);
+            // 
+            // cadastroFuncionárioToolStripMenuItem
+            // 
+            this.cadastroFuncionárioToolStripMenuItem.Name = "cadastroFuncionárioToolStripMenuItem";
+            this.cadastroFuncionárioToolStripMenuItem.Size = new System.Drawing.Size(236, 26);
+            this.cadastroFuncionárioToolStripMenuItem.Text = "Funcionáro";
+            // 
+            // cadastroLocatárioToolStripMenuItem
+            // 
+            this.cadastroLocatárioToolStripMenuItem.Name = "cadastroLocatárioToolStripMenuItem";
+            this.cadastroLocatárioToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.cadastroLocatárioToolStripMenuItem.Text = "Locatário";
+            this.cadastroLocatárioToolStripMenuItem.Click += new System.EventHandler(this.cadastroLocatárioToolStripMenuItem_Click);
+            // 
+            // cadastroLocaçãoToolStripMenuItem
+            // 
+            this.cadastroLocaçãoToolStripMenuItem.Name = "cadastroLocaçãoToolStripMenuItem";
+            this.cadastroLocaçãoToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.cadastroLocaçãoToolStripMenuItem.Text = "Locação";
+            // 
+            // editoraToolStripMenuItem
+            // 
+            this.editoraToolStripMenuItem.Name = "editoraToolStripMenuItem";
+            this.editoraToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.editoraToolStripMenuItem.Text = "Editora";
+            // 
+            // MDIParent1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(948, 617);
+            this.Controls.Add(this.statusStrip);
+            this.Controls.Add(this.menuStrip);
+            this.Font = new System.Drawing.Font("Consolas", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.IsMdiContainer = true;
+            this.MainMenuStrip = this.menuStrip;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "MDIParent1";
+            this.Text = "MDIParent1";
+            this.menuStrip.ResumeLayout(false);
+            this.menuStrip.PerformLayout();
+            this.statusStrip.ResumeLayout(false);
+            this.statusStrip.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+        #endregion
+
+
+        private System.Windows.Forms.MenuStrip menuStrip;
+        private System.Windows.Forms.StatusStrip statusStrip;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel;
+        private System.Windows.Forms.ToolTip toolTip;
+        private System.Windows.Forms.ToolStripMenuItem cadastroGeralToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem cadastroLivroToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem editarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem consultarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ajudaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem opçõesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem backupToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem restaurarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem cadastroFuncionárioToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem cadastroLocatárioToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem cadastroLocaçãoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem editoraToolStripMenuItem;
+    }
+}
+
+
+
